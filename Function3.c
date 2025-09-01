e78af2f7910f08f323d129d0c21c408bc9b49bf5
rohit88-gif <rohit1416shinde@gmail.com>
2025-09-01 18:11:49 +0530

    Add files via upload

---
+++ Function3.c
@@ -0,0 +1,30 @@
+
+
+
+
+
+
+
+
+
+void checkvoting();
+
+int main()
+{
+	checkvoting();
+}
+  void checkvoting()
+  {
+  	int age;
+  	printf("enter the age");
+  	scanf("%d",&age);
+  	
+  	if(age>18)
+  	{
+  		printf("you are voting");
+	  }
+	  else 
+	  {
+	  	printf("you are not voting");
+	  }
+  }
